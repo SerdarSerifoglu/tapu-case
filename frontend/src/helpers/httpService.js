56a6bea6b88371d3ae9ced994e923f61c8b60ae3
serdarserifoglu <serdarserifoglu@gmail.com>
2022-09-01 02:29:23 +0300

    code cleared

--- frontend/src/helpers/httpService.js
+++ frontend/src/helpers/httpService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const httpService = axios.create();
-httpService.defaults.baseURL = "http://localhost:3000/";
+httpService.defaults.baseURL = "http://localhost:3001/";
 
 export default httpService;
